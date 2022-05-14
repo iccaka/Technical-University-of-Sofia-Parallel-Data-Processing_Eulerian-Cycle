9da42f9c61d4cab8b59642314a8c995199dfdad0
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2022-05-14 18:49:47 +0300

    Add nonparallel and parallel execution examples

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import datetime
+
 from graph import Graph
+import time
+import threading
+
+
+def do_something():
+    print("hi")
+    time.sleep(0.5)
+
 
 if __name__ == '__main__':
     G = Graph([1, 2, 3, 4, 5, 6, 7], {(1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7),
@@ -7,29 +17,35 @@ if __name__ == '__main__':
                                       (4, 5), (4, 6), (4, 7),
                                       (5, 6), (5, 7),
                                       (6, 7)})
-    G1 = Graph([1, 2, 3, 4, 5], {(1, 2), (1, 3), (1, 4), (1, 5),
-                                 (2, 3), (2, 4), (2, 5),
-                                 (3, 4), (3, 5),
-                                 (4, 5)})
-    G2 = Graph([1, 2, 3, 4, 5], {(1, 2), (1, 3), (1, 4), (1, 5),
-                                 (2, 3), (4, 5)})
-    print(G.euler_tour())
-    print(G1.euler_tour())
-    print(G2.euler_tour())
 
-    G3 = Graph([1, 2, 3, 4], {(1, 2), (1, 3), (1, 4),
-                              (2, 3)})
-    print(G3.euler_tour())
+    before = datetime.datetime.now().microsecond
 
-    # G = Graph([1, 2, 3, 4], {(1, 2), (1, 3), (2, 3)})
-    # assert(len(G.V) == 4 and len(G.E) == 3 and G.deg(1) == 2)
-    #
-    # G.add_vertex(5)
-    # G.add_edge(1, 5)
-    # assert(len(G.V) == 5 and len(G.E) == 4 and G.deg(1) == 3)
-    #
-    # G.remove_vertex(5)
-    # assert(len(G.V) == 4 and len(G.E) == 3 and G.deg(1) == 2)
-    #
-    # G.remove_vertex(6)
-    # assert(len(G.V) == 4 and len(G.E) == 3)
+    print("Nonparallel way:")
+
+    for x in G.V:
+        G.euler_tour(x)
+
+    after = datetime.datetime.now().microsecond
+    difference = after - before
+
+    print(f"Time it took(nonparallel): {difference} microseconds")
+    print(f"\n=====================================\n")
+    print("Parallel way:")
+
+    threads = []
+    for x in G.V:
+        t = threading.Thread(target=G.euler_tour, args=(x, ))
+        threads.append(t)
+
+    before = datetime.datetime.now().microsecond
+
+    for x in threads:
+        x.start()
+
+    for x in threads:
+        x.join()
+
+    after = datetime.datetime.now().microsecond
+    difference = after - before
+
+    print(f"Time it took(parallel): {difference} microseconds")
